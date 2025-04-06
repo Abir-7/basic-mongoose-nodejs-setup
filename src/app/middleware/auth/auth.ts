7a34b76d09af05c1cace7b877e48ce9fb20ad6e6
Md. Tazwarul Islam Abir <121948453+Abir-7@users.noreply.github.com>
2025-04-06 14:21:10 +0600

    Update auth.ts

--- src/app/middleware/auth/auth.ts
+++ src/app/middleware/auth/auth.ts
@@ -10,40 +10,46 @@ import User from "../../modules/users/user/user.model";
 export const auth =
   (...userRole: TUserRole[]) =>
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    const tokenWithBearer = req.headers.authorization as string;
+    try {
+      const tokenWithBearer = req.headers.authorization as string;
 
-    if (!tokenWithBearer || !tokenWithBearer.startsWith("Bearer")) {
-      return next(new AppError(status.UNAUTHORIZED, "You are not authorized"));
+      if (!tokenWithBearer || !tokenWithBearer.startsWith("Bearer")) {
+        return next(new AppError(status.UNAUTHORIZED, "You are not authorized"));
+      }
+
+      const token = tokenWithBearer.split(" ")[1];
+
+      if (token === "null") {
+        return next(new AppError(status.UNAUTHORIZED, "You are not authorized"));
+      }
+
+      const decodedData = jsonWebToken.verifyJwt(
+        token,
+        appConfig.jwt.jwt_access_secret as string
+      );
+
+      const userData = await User.findById(decodedData.userId);
+
+      if (!userData) {
+        return next(new AppError(status.UNAUTHORIZED, "You are not authorized"));
+      }
+
+      if (userRole.length && !userRole.includes(decodedData.userRole)) {
+        return next(new AppError(status.UNAUTHORIZED, "You are not authorized"));
+      }
+
+      if (
+        userData.role !== decodedData.userRole ||
+        userData.email !== decodedData.userEmail
+      ) {
+        return next(new AppError(status.UNAUTHORIZED, "You are not authorized"));
+      }
+
+      req.user = decodedData;
+
+      return next();
+    } catch (error) {
+      return next(new AppError(status.UNAUTHORIZED, "Invalid or expired token"));
     }
-
-    const token = tokenWithBearer.split(" ")[1];
-
-    if (token === "null") {
-      return next(new AppError(status.UNAUTHORIZED, "You are not authorized"));
-    }
-
-    const decodedData = jsonWebToken.verifyJwt(
-      token,
-      appConfig.jwt.jwt_access_secret as string
-    );
-
-    const userData = await User.findById(decodedData.userId);
-
-    if (!userData) {
-      return next(new AppError(status.UNAUTHORIZED, "You are not authorized"));
-    }
-
-    if (userRole.length && !userRole.includes(decodedData.userRole)) {
-      return next(new AppError(status.UNAUTHORIZED, "You are not authorized"));
-    }
-
-    if (
-      userData.role !== decodedData.userRole ||
-      userData.email !== decodedData.userEmail
-    ) {
-      return next(new AppError(status.UNAUTHORIZED, "You are not authorized"));
-    }
-    req.user = decodedData;
-
-    return next();
   };
+
